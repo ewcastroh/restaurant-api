2c0f9660f4e4706b939a0571d7e5caf8e6571ed3
ewcastroh <ewcastroh10@gmail.com>
2020-08-29 00:53:18 -0500

    Refactoring in Employee entity to set all fields not nullable.

--- src/main/java/com/teaminternational/assessment/ewch/model/entity/Employee.java
+++ src/main/java/com/teaminternational/assessment/ewch/model/entity/Employee.java
@@ -26,33 +26,33 @@ public class Employee {
     @NotEmpty(message = ErrorMessages.USERNAME_NOT_EMPTY)
     private String username;
 
-    @Column(name = "date_of_birth")
+    @Column(name = "date_of_birth", nullable = false)
     @Temporal(TemporalType.DATE)
     @NotEmpty(message = ErrorMessages.DATE_OF_BIRTH_NOT_EMPTY)
     private Date dateOfBirth;
 
-    @Column(name = "hire_date")
+    @Column(name = "hire_date", nullable = false)
     @Temporal(TemporalType.DATE)
     @NotEmpty(message = ErrorMessages.HIRE_DATE_NOT_EMPTY)
     private Date hireDate;
 
-    @Column(name = "area")
+    @Column(name = "area", nullable = false)
     @NotEmpty(message = ErrorMessages.AREA_NOT_EMPTY)
     private String area;
 
-    @Column(name = "job_title")
+    @Column(name = "job_title", nullable = false)
     @NotEmpty(message = ErrorMessages.JOB_TITLE_NOT_EMPTY)
     private String jobTitle;
 
-    @Column(name = "country")
+    @Column(name = "country", nullable = false)
     @NotEmpty(message = ErrorMessages.COUNTRY_NOT_EMPTY)
     private String country;
 
-    @Column(name = "status")
+    @Column(name = "status", nullable = false)
     @NotEmpty(message = ErrorMessages.STATUS_NOT_EMPTY)
     private Boolean status;
 
-    @Column(name = "tip_rate", columnDefinition="Decimal(10,2) default '0.00'")
+    @Column(name = "tip_rate", nullable = false, columnDefinition="Decimal(10,2) default '0.00'")
     private double tipRate;
 
 }
